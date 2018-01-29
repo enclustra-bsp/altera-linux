da24fe13c5c1b775e265d7147151732cda311b61
Tomasz Gorochowik <tgorochowik@antmicro.com>
2018-01-29 15:46:07 +0100

    arch: arm: altera: move aa1 reset code to a proper callback
    This commit: 9b7f9968ab94734975f59beb9d6df71da7582e11 was supposed to
    fix the aa1 restart procedure, but the code was in an improper callback.
    
    This patch moves the code where to the proper one.

--- arch/arm/mach-socfpga/socfpga.c
+++ arch/arm/mach-socfpga/socfpga.c
@@ -185,10 +185,6 @@ static void socfpga_cyclone5_restart(enum reboot_mode mode, const char *cmd)
 	/* Turn on all periph PLL clocks */
 	writel(0xffff, clkmgr_base_addr + SOCFPGA_ENABLE_PLL_REG);
 
-	temp = readl(sys_manager_base_addr + SOCFPGA_A10_SYSMGR_ROMCODE_CTRL);
-	temp |= ROMCODE_CTRL_WARMRSTCFGIO | ROMCODE_CTRL_WARMRSTCFGPINMUX;
-	writel(temp, sys_manager_base_addr + SOCFPGA_A10_SYSMGR_ROMCODE_CTRL);
-
 	temp = readl(rst_manager_base_addr + SOCFPGA_RSTMGR_CTRL);
 
 	if (mode == REBOOT_HARD)
@@ -201,6 +197,10 @@ static void socfpga_cyclone5_restart(enum reboot_mode mode, const char *cmd)
 static void socfpga_arria10_restart(enum reboot_mode mode, const char *cmd)
 {
 	u32 temp;
+
+	temp = readl(sys_manager_base_addr + SOCFPGA_A10_SYSMGR_ROMCODE_CTRL);
+	temp |= ROMCODE_CTRL_WARMRSTCFGIO | ROMCODE_CTRL_WARMRSTCFGPINMUX;
+	writel(temp, sys_manager_base_addr + SOCFPGA_A10_SYSMGR_ROMCODE_CTRL);
 
 	temp = readl(rst_manager_base_addr + SOCFPGA_A10_RSTMGR_CTRL);
 
